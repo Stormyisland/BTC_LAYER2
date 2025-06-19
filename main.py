9133f9505e866e527789cf8f18322a0768fcc330
Jacques Lavoie <46101717+Stormyisland@users.noreply.github.com>
2025-06-19 13:46:15 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,14 @@ from dataclass import dataclass
 import time
 class Blockchain:
   def __init__self:
+      self.contracts = {}
+
+  def deploy_contract(self, contract_code: str):
+    contract_address = hashlib.sha256(contract_code.encode()).hexdigest()[:20]
+    self.coontracts[contact_address] = contacr_code 
+    return ccontract_address 
+
+  def call_contract(self, address: str, function: str,args: dict):
+      print(f"Blockchain: Calling {function} on address} with {args}")
+    return True
   
